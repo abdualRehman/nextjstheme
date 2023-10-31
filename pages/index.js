366dc813766f85cc30e0272cc5b5d401432d1914
abdulrehman_ <abdul.rahman@nexrupt.com>
2023-10-31 18:53:24 +0500

    pagination added

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 // mui
-import { Box } from '@mui/material'
+import { Box } from "@mui/material";
 
 // component
-import Category from '../components/Category'
-import Slider from '../components/Slider'
-import ProductSliderComponent from '../components/Product-Slider'
-import BannerComponent from '../components/BannerComponent'
-import Image from 'next/image'
-import logo from '../public/images/logo2.png'
+import Category from "../components/Category";
+import Slider from "../components/Slider";
+import ProductSliderComponent from "../components/Product-Slider";
+import BannerComponent from "../components/BannerComponent";
+import Image from "next/image";
+import logo from "../public/images/logo2.png";
 
 export default function Home() {
-
   return (
     <Box>
-
       <Box>
         <Slider />
       </Box>
-
       <Box>
         <Category />
       </Box>
-
       <Box>
         <ProductSliderComponent category={"Mobiles"} />
       </Box>
@@ -33,10 +29,9 @@ export default function Home() {
       <Box>
         <ProductSliderComponent category={"Headsets"} />
       </Box>
-      <Box sx={{ width: "100%", margin: "auto", textAlign: "center", my: 10 }} >
+      <Box sx={{ width: "100%", margin: "auto", textAlign: "center", my: 10 }}>
         <Image src={logo} />
       </Box>
-
     </Box>
-  )
+  );
 }
